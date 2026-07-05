3df0cb2bcbd8cbdc88dd679c05168a98d3f7c821
gamath99 <gamamathurin1@gmail.com>
2026-07-05 18:48:23 -0400

    Final file for the weekly prompt

---
+++ prompt_5.py
@@ -0,0 +1,14 @@
+"""
+Weekly prompt unit 5 
+Gama MATHURIN 
+
+Explain how testing code improves your written code. 
+
+In programming, testing code is a necessary part of the process to verify the execution of the code.
+The process also helps to identify errors, exposes mistakes, forces to write cleaner code and more maintainable code. 
+Along the process, it is recommended to have different sections while writing the code, it helps to check different inputs and situations to locate bugs before the program is completed.
+By testing, it clarifies what exactly the expectations should be and prevents futures mistakes by ordering the codes along the process. It proves to be essential to have great maintenance of the code, easy to detect errors 
+and facilitate modification at the exact line needed. Moreover, by testing along the way, it avoids the chance of larger issues later and helps to save time. 
+At the same time, regular testing provides more motivation for the programmer by improving confidence and speed. It also offers the opportunity to evaluate the program from the user’s point of view. 
+Overall, testing helps to improve the written code, by generating high quality code, make it maintainable, accurate and reliable. 
+"""
